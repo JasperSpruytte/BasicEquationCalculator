110949438aba75d95b5faf0830d602ff5afc346d
Jasper Spruytte <jaspergj.spruytte@gmail.com>
2014-09-23 18:57:15 +0200

    Started project

---
+++ EquationCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace BasicEquationCalculator
+{
+    public class EquationCalculator
+    {
+        public Point CalculateIntersection(string equation1, string equation2)
+        {
+            Regex regex = new Regex(@"", RegexOptions.IgnoreCase);
+
+            return new Point(1,3);
+        }
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicEquationCalculator
+{
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            Point objPoint = obj as Point;
+
+            if (objPoint == null)
+                return false;
+
+            return X == objPoint.X && Y == objPoint.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X ^ Y;
+        }
+    }
+}
